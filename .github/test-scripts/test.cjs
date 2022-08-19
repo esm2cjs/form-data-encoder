bf8d4de9b01f02bf4d6bef3ea7d350da33c34c1f
AlCalzone <d.griesel@gmx.net>
2022-08-19 09:51:52 +0200

    feat: convert to hybrid package (#1)

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const { FormDataEncoder } = require("@esm2cjs/form-data-encoder");
+const assert = require("assert");
+
+assert(typeof FormDataEncoder === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import { FormDataEncoder } from "@esm2cjs/form-data-encoder";
+import assert from "assert";
+
+assert(typeof FormDataEncoder === "function");
